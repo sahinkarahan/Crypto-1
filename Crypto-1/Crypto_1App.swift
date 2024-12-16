56820a48a335df8051a53eb3b525c3b2b7214f19
sahinkarahan <sahin.karahan@hotmail.com>
2024-12-16 16:13:13 +0300

    Initial Commit

---
+++ Crypto-1/Crypto_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_1App.swift
+//  Crypto-1
+//
+//  Created by Şahin Karahan on 16.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
